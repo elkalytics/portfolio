2d1755e50e6538a5ad6eca94907f85edd5e93170
elkalytics <124639173+elkalytics@users.noreply.github.com>
2023-02-08 20:08:01 -0500

    Added helpers
    Added functions to import and help profile data

---
+++ helper_functions/unique_field_profile.R
@@ -0,0 +1,33 @@
+unique_fields_df <- function(df_list) {
+  df_names <- names(df_list)
+  field_names <- unique(unlist(lapply(df_list, names)))
+  
+  exists_matrix <- matrix(nrow = length(df_list), ncol = length(field_names))
+  colnames(exists_matrix) <- field_names
+  
+  for (i in 1:length(df_list)) {
+    for (j in 1:length(field_names)) {
+      exists_matrix[i, j] <- ifelse(field_names[j] %in% names(df_list[[i]]), 1, 0)
+    }
+  }
+  
+  result_df <- as.data.frame(exists_matrix)
+  result_df$df_name <- df_names
+  
+  return(result_df)
+}
+
+
+# Create fake data
+example_data_1 <- data.frame(A = 1:5, B = 6:10, C = 11:15)
+example_data_2 <- data.frame(A = 11:15, B = 16:20, D = 21:25)
+example_data_3 <- data.frame(C = 31:35, D = 36:40, E = 41:45)
+
+# Save data as a list
+df_list <- list(example_data_1, example_data_2, example_data_3)
+
+# Use the function to save the results
+unique_fields_df(df_list) -> results
+
+# Review
+View(results)
