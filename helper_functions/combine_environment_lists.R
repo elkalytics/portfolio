9ee2476bfe58bf7099d1400dc032457ad17bc0c6
elkalytics <124639173+elkalytics@users.noreply.github.com>
2023-02-18 03:12:00 -0500

    Updated list merge function
    Updated function to account for data structure

--- helper_functions/combine_environment_lists.R
+++ helper_functions/combine_environment_lists.R
@@ -1,12 +1,22 @@
-# Combine all lists into a single list
 
-combine_lists <- function() {
-  # Get all the objects in the environment that are lists
-  lists <- Filter(is.list, mget(ls()))
-  
-  # Merge the lists into a single list of lists
-  combined_list <- do.call(c, lists)
-  
-  return(combined_list)
+# Load library
+library(dplyr)
+
+# Load data
+box_office_all <- readRDS("box_office_all.RDS")
+
+# Define a function to convert each nested data frame into a character data frame
+nested_to_char <- function(nested_df, date) {
+  as.data.frame(lapply(nested_df, as.character), stringsAsFactors = FALSE) %>%
+    mutate(date = rep(date, nrow(.)))
 }
+
+# Apply the function to each element of the nested list and bind the resulting data frames
+df_list <- lapply(box_office_all, function(nested_list) {
+  lapply(nested_list, function(nested_df) {
+    nested_to_char(nested_df, names(nested_list)[1])
+  }) %>%
+    bind_rows()
+}) %>%
+  bind_rows()
 
