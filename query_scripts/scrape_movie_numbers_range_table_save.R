6cdd0f4627764f3affceba7f09a7969afbc527dd
elkalytics <124639173+elkalytics@users.noreply.github.com>
2023-02-18 19:06:47 -0500

    Box office query
    Added extended timers and catch to prevent/reduce timeouts.

--- query_scripts/scrape_movie_numbers_range_table_save.R
+++ query_scripts/scrape_movie_numbers_range_table_save.R
@@ -14,24 +14,37 @@ scrape_box_office_range <- function(start_date, end_date) {
     # Construct URL for the given date
     url <- paste0("https://www.the-numbers.com/box-office-chart/daily/", dates[i])
     
-    # If the year changes, add an extra 10 second pause
+    # If the year changes, add an extra 30 second pause
     if (i > 1 && substr(dates[i], 1, 4) != substr(dates[i-1], 1, 4)) {
-      Sys.sleep(10)
+      Sys.sleep(30)
     }
     
-    # Scrape the table using CSS selectors
-    webpage <- read_html(url)
-    table <- html_table(html_nodes(webpage, "table")[2], fill = TRUE)
+    # Try to scrape the table using CSS selectors up to 5 times in case of a timeout
+    tries <- 1
+    while (tries <= 5) {
+      tryCatch({
+        webpage <- read_html(url)
+        table <- html_table(html_nodes(webpage, "table")[2], fill = TRUE)
+        # Convert list to data frame and add table date column
+        table_df <- as.data.frame(table) %>%
+          mutate(table_date = dates[i])
+        # Bind table to previous tables
+        box_office_data <- bind_rows(box_office_data, table_df)
+        # Exit loop if successful
+        break
+      }, error = function(cond) {
+        # Pause for 30 seconds before next try
+        Sys.sleep(30)
+        tries <<- tries + 1
+        # If max number of tries has been reached, stop and return error
+        if (tries > 5) {
+          stop("Failed to retrieve data after multiple attempts")
+        }
+      })
+    }
     
-    # Convert list to data frame and add table date column
-    table_df <- as.data.frame(lapply(table, as.character)) %>%
-      mutate(table_date = dates[i])
-    
-    # Bind table to previous tables
-    box_office_data <- bind_rows(box_office_data, table_df)
-    
-    # Pause for 1 second before making the next request
-    Sys.sleep(1)
+    # Pause for 10 second before making the next request
+    Sys.sleep(10)
   }
   
   return(box_office_data)
