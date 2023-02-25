5fb0c7cad8b26a067b73b0c1f8e313c68b506628
elkalytics <124639173+elkalytics@users.noreply.github.com>
2023-02-25 13:11:01 -0500

    Adstock grid parallel
    Improve original grid search adstock function, and made a combined function that allows parallel processing for larger data.

--- helper_functions/adstock_grid.R
+++ helper_functions/adstock_grid.R
@@ -1,37 +1,65 @@
 # Grid search function to be used with adstock function
 # Searches for optimal parameters
 
+# Load packages
+library(dplyr)
+library(doParallel)
+library(foreach)
+library(memoise)
+library(future)
+library(zoo)
+library(furrr)
+
+# Adstock
+adstock <- function(x, lag, decay) {
+  # Apply adstock transformation to a vector x
+  n <- length(x)
+  weights <- (1:lag) * decay
+  weights <- weights / sum(weights)
+  adstocked <- rep(0, n)
+  for (i in lag:n) {
+    adstocked[i] <- sum(x[(i-lag+1):i] * weights)
+  }
+  adstocked
+}
+
 # Define a function to calculate the sum of squared errors between the predicted and actual values
 sse <- function(actual, predicted) {
+  # Calculate the sum of squared errors between the predicted and actual values
   sum((actual - predicted)^2)
 }
 
 # Define a function to perform the grid search for optimal lag and decay values
 adstock_grid_search <- function(x, lags, decays) {
-  sse_values <- matrix(nrow = length(lags), ncol = length(decays))
-  for (i in 1:length(lags)) {
-    for (j in 1:length(decays)) {
-      adstocked <- adstock(x, lags[i], decays[j])
-      sse_values[i,j] <- sse(x, adstocked)
+  # Perform a grid search to find optimal lag and decay values for adstock transformation
+  n_lags <- length(lags)
+  n_decays <- length(decays)
+  sse_values <- matrix(nrow = n_lags, ncol = n_decays)
+  adstocked_values <- array(NA, dim = c(length(x), n_lags, n_decays))
+  for (i in 1:n_lags) {
+    for (j in 1:n_decays) {
+      if (is.na(adstocked_values[1,i,j])) {
+        adstocked_values[,i,j] <- adstock(x, lags[i], decays[j])
+      }
+      sse_values[i,j] <- sse(x, adstocked_values[,i,j])
     }
   }
-  results <- list(lags = lags, decays = decays, sse = sse_values)
-  return(results)
+  list(lags = lags, decays = decays, sse = sse_values)
 }
 
-# Example usage
-x <- c(10, 20, 30, 40, 50)
-lags <- seq(1, 3, by = 1)
-decays <- seq(0.1, 0.9, by = 0.1)
-results <- adstock_grid_search(x, lags, decays)
+## Example usage
+# x <- c(10, 20, 30, 40, 50)
+# lags <- seq(1, 3, by = 1)
+# decays <- seq(0.1, 0.9, by = 0.1)
+# results <- adstock_grid_search(x, lags, decays)
 
-# Print the results of the grid search
-print(results)
+## Print the results of the grid search
+# print(results)
 
-# Find the optimal lag and decay values that minimize SSE
-min_sse <- min(results$sse)
-optimal_indices <- which(results$sse == min_sse, arr.ind = TRUE)
-optimal_lag <- results$lags[optimal_indices[1]]
-optimal_decay <- results$decays[optimal_indices[2]]
-cat("Optimal lag:", optimal_lag, "\n")
-cat("Optimal decay:", optimal_decay, "\n")
+## Find the optimal lag and decay values that minimize SSE
+# min_sse <- min(results$sse)
+# optimal_indices <- which(results$sse == min_sse, arr.ind = TRUE)
+# optimal_lag <- results$lags[optimal_indices[1]]
+# optimal_decay <- results$decays[optimal_indices[2]]
+# cat("Optimal lag:", optimal_lag, "\n")
+# cat("Optimal decay:", optimal_decay, "\n")
---
+++ helper_functions/adstock_grid_vector_combined.R
@@ -0,0 +1,67 @@
+# Combined function for adstock grid
+# Vectorized and parallel processing for larger data
+
+# Load packages
+library(parallel)
+library(memoise)
+
+# Save function
+adstock_vectorized <- function(x, lag, decay) {
+  # Apply adstock transformation to a vector x using vectorized operations
+  weights <- seq(1, lag) * decay
+  weights <- weights / sum(weights)
+  rollapply(x, width = lag, FUN = function(x) sum(x * weights), align = "right", fill = NA)
+}
+
+sse <- function(actual, predicted) {
+  # Calculate the sum of squared errors between the predicted and actual values
+  sum((actual - predicted)^2)
+}
+
+adstock_grid_search <- function(x, lags, decays, cores = 1) {
+  # Perform a grid search to find optimal lag and decay values for adstock transformation
+  adstocked_values <- array(NA_real_, dim = c(length(x), length(lags), length(decays)))
+  
+  # Define a memoized version of the adstock function to reuse computed values
+  adstock_memo <- memoise(function(x, lag, decay) {
+    if (is.na(adstocked_values[1, which(lags == lag), which(decays == decay)])) {
+      adstocked_values[, which(lags == lag), which(decays == decay)] <<- adstock_vectorized(x, lag, decay)
+    }
+    adstocked_values[, which(lags == lag), which(decays == decay)]
+  })
+  
+  # Perform grid search using parallel processing
+  cl <- makeCluster(cores)
+  clusterEvalQ(cl, {
+    library(zoo)
+    library(memoise)
+    adstock_vectorized <- function(x, lag, decay) {
+      # Apply adstock transformation to a vector x using vectorized operations
+      weights <- seq(1, lag) * decay
+      weights <- weights / sum(weights)
+      rollapply(x, width = lag, FUN = function(x) sum(x * weights), align = "right", fill = NA)
+    }
+    sse <- function(actual, predicted) {
+      # Calculate the sum of squared errors between the predicted and actual values
+      sum((actual - predicted)^2)
+    }
+  })
+  registerDoParallel(cl)
+  sse_values <- foreach(lag = lags, .combine = "cbind") %:%
+    foreach(decay = decays, .combine = "c") %dopar% {
+      predicted <- adstock_memo(x, lag, decay)
+      sse(x, predicted)
+    }
+  stopCluster(cl)
+  sse_values <- matrix(sse_values, nrow = length(lags), ncol = length(decays))
+  list(lags = lags, decays = decays, sse = sse_values)
+}
+
+## Make fake data
+# set.seed(123)
+# x <- rnorm(100, mean = 10, sd = 3)
+# lags <- seq(1, 10, by = 1)
+# decays <- seq(0.1, 0.9, by = 0.1)
+
+## Save results
+# results <- adstock_grid_search(x, lags, decays, cores = 2)
