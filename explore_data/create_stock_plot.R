ba6611c2a1e325a71def9a17e0ad03f22fdb7adb
elkalytics <124639173+elkalytics@users.noreply.github.com>
2023-02-28 20:49:08 -0500

    Stock plot function
    Bar plot and line with linear trend overlay

---
+++ explore_data/create_stock_plot.R
@@ -0,0 +1,47 @@
+# Function to plot revenue - barplot by year and cumulative sales over time
+
+# Load packages
+library(ggplot2)
+library(ggpubr)
+library(ggpmisc)
+
+# Save function
+create_stock_plot <- function(data, year_var, revenue_var) {
+  # Convert data to a data frame
+  data_df <- data.frame(data)
+  
+  # Calculate stock values
+  stock_values <- cumsum(data_df[[revenue_var]])
+  
+  # Add stock values to data frame
+  data_df$stock_value <- stock_values
+  
+  # Create boxplot
+  boxplot <- ggplot(data_df, aes(x = as.factor(.data[[year_var]]), y = .data[[revenue_var]])) +
+    geom_boxplot(fill = "white", color = "gray50") +
+    scale_y_continuous(labels = scales::dollar_format()) +
+    labs(x = "Year", y = "Revenue", title = "Revenue Boxplot by Year") +
+    theme_classic()
+  
+  # Create stock plot with trend line and formula
+  stock_plot <- ggplot(data_df, aes(x = .data[[year_var]], y = stock_value)) +
+    geom_line(color = "steelblue", size = 1.2) +
+    geom_smooth(method = "lm", formula = y ~ x, se = FALSE, color = "orange") +
+    stat_poly_eq(formula = y ~ x, aes(label = paste(after_stat(eq.label), after_stat(rr.label), sep = "~~~")), 
+                 parse = TRUE, size = 5, color = "black") +
+    scale_y_continuous(labels = scales::dollar_format()) +
+    labs(x = "Year", y = "Cumulative Revenue", title = "Stock Plot of Cumulative Revenue by Year") +
+    theme_classic()
+  
+  # Combine boxplot and stock plot
+  combined_plot <- ggarrange(boxplot, stock_plot, ncol = 1, heights = c(0.4, 0.6))
+  
+  return(combined_plot)
+}
+
+## Make a fake data set
+# revenue_data <- data.frame(year = c(2018, 2018, 2019, 2019, 2020, 2020, 2021, 2021),
+#                            sales = c(50, 150, 100, 200, 75, 75, 200, 100))
+
+## Apply function
+# create_stock_plot(data = revenue_data, year_var = "year", revenue_var = "sales")
