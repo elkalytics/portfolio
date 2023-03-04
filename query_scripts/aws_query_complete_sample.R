da4871eb7df08a89799ca30d382b995ab1766772
elkalytics <124639173+elkalytics@users.noreply.github.com>
2023-03-04 09:58:10 -0500

    AWS sample query
    Function to query first 10 complete/mostly complete rows from AWS using R.

---
+++ query_scripts/aws_query_complete_sample.R
@@ -0,0 +1,73 @@
+# Query sample of first 10 non-null rows from AWS
+# If all the rows have at least 1 null, it will take the most complete rows
+
+library(RMySQL)
+
+# function to query first 10 rows of all tables without nulls or the 10 most complete rows
+query_first_10_rows <- function(dbname, user, password, host) {
+  # create database connection
+  con <- dbConnect(MySQL(), dbname = dbname, user = user, password = password, host = host)
+  
+  # check if connection was successful
+  if (is.null(con)) stop("Error: Could not connect to database")
+  
+  # get list of tables in database
+  tables <- dbListTables(con)
+  
+  # check if any tables were found
+  if (length(tables) == 0) stop("Error: No tables found in database")
+  
+  # create empty list to store results
+  results <- vector(mode = "list", length = length(tables))
+  
+  # loop through tables and query the first 10 rows without nulls or the 10 most complete rows
+  for (i in seq_along(tables)) {
+    table <- tables[i]
+    
+    # get list of columns in table
+    cols <- dbListFields(con, table)
+    
+    # create query to select rows without nulls in any column
+    query_without_nulls <- sprintf("SELECT * FROM `%s` WHERE %s LIMIT 10",
+                                   table, paste0("`", cols, "`", " IS NOT NULL", collapse = " AND "))
+    
+    # create query to select the 10 most complete rows in table
+    query_most_complete <- sprintf("SELECT * FROM `%s` WHERE (%s) ORDER BY %s LIMIT 10",
+                                   table, paste0("`", cols, "`", " IS NOT NULL", collapse = " OR "),
+                                   paste0("(`", cols, "`", " IS NOT NULL) DESC", collapse = ", "))
+    
+    # try to query rows without nulls
+    result <- dbGetQuery(con, query_without_nulls)
+    
+    # if there are nulls in every column, query the 10 most complete rows
+    if (is.null(result)) {
+      result <- dbGetQuery(con, query_most_complete)
+      
+      # if both queries fail, print a warning
+      if (is.null(result)) {
+        warning(sprintf("Warning: Could not query table %s", table))
+      }
+    }
+    
+    # add result to list if query was successful
+    if (!is.null(result)) {
+      results[[i]] <- setNames(list(result), table)
+    }
+  }
+  
+  # close database connection
+  dbDisconnect(con)
+  
+  # return list of results
+  results <- Filter(Negate(is.null), results)
+  if (length(results) == 0) return(NULL)
+  names(results) <- tables[names(results)]
+  return(results)
+}
+
+# example usage
+dbname <- "mydatabase"
+user <- "myuser"
+password <- "mypassword"
+host <- "mydatabase.us-west-2.rds.amazonaws.com"
+results <- query_first_10_rows(dbname, user, password, host)
