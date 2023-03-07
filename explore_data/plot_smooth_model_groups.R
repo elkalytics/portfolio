a451d95aea7e3decb92efa85cbd655792a8b97b2
elkalytics <124639173+elkalytics@users.noreply.github.com>
2023-03-06 20:06:55 -0500

    Added library
    Added library

--- explore_data/plot_smooth_model_groups.R
+++ explore_data/plot_smooth_model_groups.R
@@ -16,6 +16,7 @@
 #' plot_smooth_group(mpg, "displ", "hwy", color = NULL, facet = "cyl", method = "loess")
 #' plot_smooth_group(mpg, "displ", "hwy", color = NULL, facet = "cyl", method = "gam")
 #' plot_smooth_group(mpg, "displ", "hwy", color = NULL, facet = "cyl", method = "glm")
+library(ggplot2)
 plot_smooth_group <- function(data, x, y, color, facet, method) {
   ggplot(data, aes_string(x, y, color = color)) +
     geom_point() +
